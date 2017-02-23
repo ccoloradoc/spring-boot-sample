393aa41cfe106c32875f333a7585e5048073b16e
Cristian Colorado <ccoloradoc@gmail.com>
2017-02-23 11:17:48 -0600

    First Commit: Simple Web App + Webjars

---
+++ src/main/java/mx/wedevelop/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package mx.wedevelop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by colorado on 22/02/17.
+ */
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
